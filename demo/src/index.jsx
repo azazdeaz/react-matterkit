8b9b8c5cbca5c23db6cbb794b8de7e7d1e5da270
Polgar <azazdeaz@gmail.com>
2015-03-22 23:10:27 +0100

    work on demo

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -2,6 +2,12 @@ var React = require('react');
 var Matter = require('../../');
 var LiveEditor = require('./react-live-edit/live-editor.jsx');
 
+var routes = {
+  Button: require('./routes/Button.jsx'),
+};
+
+console.log('routes', routes)
+
 global.Matter = Matter;
 global.React = React;
 //
