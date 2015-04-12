6774a51c19bbc9393dce9e5f85f284aa14ab5699
Polgar <azazdeaz@gmail.com>
2015-04-12 18:29:45 +0200

    init <Toggle/>

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,55 @@
+var React = require('react/addons');
+var { StyleResolverMixin, BrowserStateMixin } = require('radium');
+var style = require('./style');
+var has = require('lodash/object/has');
+var assign = require('lodash/object/assign');
+
+var Toggle = React.createClass({
+
+  mixins: [ StyleResolverMixin, BrowserStateMixin ],
+
+  getDefaultProps() {
+    return {
+      labelLeft: 'OFF',
+      valueLeft: false,
+      labelRight: 'ONN',
+      valueRight: true,
+    };
+  },
+
+  getInitialState() {
+    return {
+      left: true,
+    };
+  },
+
+  onClick() {
+
+    var left = !this.state.left;
+
+    if (this.props.onChange) {
+      let value = left ? this.props.valueLeft : this.props.valueRight;
+      this.props.onChange(value);
+    }
+
+    this.setState({left});
+  },
+
+  render() {
+    return <div
+      {...this.getBrowserStateEvents()}
+      style={style.toggleBase}
+      onClick={this.onClick}>
+
+      <div style={this.buildStyles(style.toggleSide, {left: true})}>
+        {this.props.labelLeft}
+      </div>
+      <div style={this.buildStyles(style.toggleSide, {right: true})}>
+        {this.props.labelRight}
+      </div>
+      <div style={this.buildStyles(style.toggleKnob)}/>
+    </div>;
+  }
+});
+
+module.exports = Toggle;
