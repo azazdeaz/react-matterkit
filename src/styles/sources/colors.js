4f9f8dfa1c647295cd5cb17108fc74e89a253dd7
Polgár András <azazdeaz@gmail.com>
2015-06-08 16:51:55 +0200

    add missing file

---
+++ src/styles/sources/colors.js
@@ -0,0 +1,14 @@
+export default function () {
+
+  return {
+    purple: '#8091c6',
+    blue: '#6bb6c4',
+    right: '#43aa81',
+    wrong: '#b64d65',
+    grey1: '#e2e7eb',
+    grey2: '#96a6ad',
+    grey3: '#3b424a',
+    grey4: '#1a1d21',
+    bg: '#262A2E',
+  };
+}
